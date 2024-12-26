0c7df5aa919b9e764c6424036f5597918edce9b7
yaver <yaverms10@gmail.com>
2024-12-26 19:44:19 +0400

    Updated Code

--- tapaz_scraping.py
+++ tapaz_scraping.py
@@ -150,6 +150,8 @@ def scrape(filtered_url):
             chrome_options.add_argument('--disable-dev-shm-usage')
             chrome_options.add_argument('--no-sandbox')
             chrome_options.add_argument('--remote-debugging-pip')
+            chrome_options.add_argument('--remote-debugging-port=8000')
+            chrome_options.add_argument('--disable-gpu')
 
             # service = ChromeService(ChromeDriverManager().install(), log_path=os.devnull)
             driver = webdriver.Chrome(service = ChromeService(ChromeDriverManager().install()), options=chrome_options)
